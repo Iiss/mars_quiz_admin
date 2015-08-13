6ffc56ccd72d4c3b642efbc538ceeede1d3eb9f2
Ilya Alisov <ia13@bk.ru>
2015-08-13 17:59:38 +0300

    Admin profile editor added

--- app/decorators.py
+++ app/decorators.py
@@ -13,5 +13,5 @@ def permission_required(permission):
 		return decorated_function
 	return decorator
 
-def admin_required(f)
+def admin_required(f):
 	return permission_required(Permission.ADMINISTER)(f)
--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,23 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
+ValidationError, SelectField
+from wtforms.validators import Required, Email, Length, Regexp, EqualTo
+from ..models import User, Role
+		
+class EditProfileAdminForm(Form):
+	email = StringField('Email', validators = [Required(), Length(1, 64), Email()])
+	name = StringField('Name', validators = [Required(), Length(1, 64)])
+	surname = StringField('Surname', validators = [Required(),Length(1,64)])
+	role = SelectField('Role', coerce = int)
+	submit = SubmitField('Submit')
 
-class NameForm(Form):
-    name = StringField('What is your name?', validators=[Required()])
-    submit = SubmitField('Submit')
+	def __init__(self, user, *args, **kwargs):
+		super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+		self.role.choices = [(role.id, role.name)
+							for role in Role.query.order_by(Role.name).all()]
+		self.user = user
+		
+	def validate_email(self, field):
+		if field.data != user.email and \
+				User.query.filter_by(email = field.data).first():
+			raise ValidationError('Email is already registered')
