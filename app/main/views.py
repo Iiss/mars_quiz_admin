362cf7d2a89755b9756686ece62e5e6617ed0247
Ilya Alisov <ia13@bk.ru>
2015-08-25 18:27:55 +0300

    quiz list template added

--- app/main/views.py
+++ app/main/views.py
@@ -3,10 +3,10 @@ from flask import render_template, session, redirect, url_for, current_app, \
 flash
 from flask.ext.login import login_required, current_user
 from . import main
-from .forms import EditProfileAdminForm, EditProfileForm
+from .forms import EditProfileAdminForm, EditProfileForm, CreateQuizForm
 from .. import db
-from ..models import User, Role
-from ..decorators import admin_required
+from ..models import User, Role, Permission, Quiz
+from ..decorators import admin_required, permission_required
 
 @main.route('/',methods=['GET','POST'])
 def index():
@@ -58,3 +58,30 @@ def edit_profile_admin(id):
     form.phone_number.data = user.phone_number
     form.organization.data = user.organization
     return render_template('edit_profile.html', form = form, user = user)
+
+''' Quiz operations views '''
+@main.route('/quiz')
+@login_required
+def show_quiz_list():
+    form = None
+    if current_user.is_administrator():
+        quiz_list = Quiz.query.all()
+    else:
+        quiz_list = current_user.quiz_list.all()
+    if current_user.can(Permission.MANAGE_QUIZ):
+        form = CreateQuizForm()
+    return render_template('quiz_list.html', quiz_list = quiz_list, form = form)
+
+@main.route('/add-quiz', methods = ['POST'])
+@login_required
+@permission_required(Permission.MANAGE_QUIZ)
+def add_quiz():
+    form = CreateQuizForm()
+    user = User.query.get(current_user.id)
+    if form.validate_on_submit():
+        quiz = Quiz()
+        quiz.title = form.title.data
+        quiz.executives.append(user)
+        db.session.add(quiz)
+        flash('Quiz "%s" succsessfully created.' % quiz.title)
+        return redirect(url_for('main.show_quiz_list'))
